082a4357f864942c3db5f09d732802873a02844d
Jose Benavente <60740213+JoseFL1999@users.noreply.github.com>
2020-02-06 11:13:03 -0300

    Ejercicio uno
    Aprendi alert var ,,,

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,22 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	alert("ok");
+	var primerPrecio;
+	var segundoPrecio;
+	var apellido="nadie";
+
+	apellido="perez";
+	alert(apellido );	
+
+
+	//Si la variable no existe no muestra nada
+	//alert(preciopri);
+	//es un valor literal si esta entre ""
+	//alert("Tal cual");
+    //alert(apellido);
+    //alert("apellido");
+  
+    alert();
+
 }
 
