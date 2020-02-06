b56d79b316ea3a64a4864a153e5a843fe00403ad
Jose Benavente <60740213+JoseFL1999@users.noreply.github.com>
2020-02-06 11:55:38 -0300

    Aprender a concatenar
    Aprendi usar console.log y prompt

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,10 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	var nombre;
+
+	nombre = prompt("Please enter your name");
+	console.log("Su nombre es " +nombre)
+    console.log("Su nombres" + nombre)
 }
 
