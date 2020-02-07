885af8043b7bc4312a68468ea309ed06796abb93
Jose Benavente <60740213+JoseFL1999@users.noreply.github.com>
2020-02-07 10:50:36 -0300

    DIVIDIR MULTIPLICAR ETC

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -5,20 +5,69 @@ ej.: "la Resta es 750"*/
 function sumar()
 {	
 	
+	var primerNumero;
+	var segundoNumero;
+	var resultadoDeLaSuma;
+
+	primerNumero= document.getElementById('numeroUno').value;
+	primerNumero= parseInt(primerNumero);
+
+	segundoNumero= document.getElementById('numeroDos').value;
+	segundoNumero= parseInt(segundoNumero);
+
+	resultadoDeLaSuma= primerNumero + segundoNumero;
+	alert("El resultado de la suma es " +resultadoDeLaSuma);
+
 }
 
 function restar()
 {
-	
+	var primerNumero;
+	var segundoNumero;
+	var resultadoDeLaSuma;
+
+	primerNumero= document.getElementById('numeroUno').value;
+	primerNumero= parseInt(primerNumero);
+
+	segundoNumero= document.getElementById('numeroDos').value;
+	segundoNumero= parseInt(segundoNumero);
+
+	resultadoDeLaSuma= primerNumero - segundoNumero;
+	alert("El resultado de la resta es " +resultadoDeLaSuma);
+
 }
 
 function multiplicar()
 { 
-	
+	var primerNumero;
+	var segundoNumero;
+	var resultadoDeLaSuma;
+
+	primerNumero= document.getElementById('numeroUno').value;
+	primerNumero= parseInt(primerNumero);
+
+	segundoNumero= document.getElementById('numeroDos').value;
+	segundoNumero= parseInt(segundoNumero);
+
+	resultadoDeLaSuma= primerNumero * segundoNumero;
+	alert("El resultado de la multiplicacion es " +resultadoDeLaSuma);
+
 }
 
 function dividir()
 {
-	
+	var primerNumero;
+	var segundoNumero;
+	var resultadoDeLaSuma;
+
+	primerNumero= document.getElementById('numeroUno').value;
+	primerNumero= parseInt(primerNumero);
+
+	segundoNumero= document.getElementById('numeroDos').value;
+	segundoNumero= parseInt(segundoNumero);
+
+	resultadoDeLaSuma= primerNumero / segundoNumero;
+	alert("El resultado de la division es " +resultadoDeLaSuma);
+
 }
 
