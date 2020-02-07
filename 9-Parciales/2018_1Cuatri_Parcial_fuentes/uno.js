e4ea53ba69d042533b5f100481aaff5c3bec675c
Jose Benavente <60740213+JoseFL1999@users.noreply.github.com>
2020-02-07 18:46:33 -0300

    Ejercicio 1 parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,20 @@
 
 function mostrar()
 {
+	var anchoRectangulo;
+	var largoRectangulo;
+	var perimetroRectangulo;
+
+	anchoRectangulo= prompt("Escriba el ancho del rectangulo");
+	console.log("El ancho del rectangulo es "+ anchoRectangulo);
+
+	largoRectangulo= prompt("Escriba el largo del rectangulo ");
+	console.log("El largo del rectangulo es "+ largoRectangulo);
+
+	perimetroRectangulo= 2*anchoRectangulo + 2*largoRectangulo;
+	alert("El perimetro del rectangulo es "+ perimetroRectangulo);
+
+
+
 
 }
