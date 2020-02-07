4b490a6b6bd042f1599dfbfac0766735f71baf67
Jose Benavente <60740213+JoseFL1999@users.noreply.github.com>
2020-02-07 10:11:28 -0300

    Aprendiendo el parseint

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,31 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+	var primerNumero;
+	var segundoNumero;
+	var suma;
+	//primerNumero=2;
+	//segundoNumero=3;
+	primerNumero=document.getElementById('numeroUno').value;
+	console.log(primerNumero);
+	primerNumero=parseInt(primerNumero);
+	console.log(primerNumero);
+	/*primerNumero=parseInt("33");
+	primerNumero=parseInt("lalala");
+	primerNumero=parseInt("m33");
+	primerNumero=parseInt("33m");
+	primerNumero=parseInt("3m3m");*/
+
+
+
+
+	segundoNumero=document.getElementById('numeroDos').value;
+	console.log(segundoNumero);
+	segundoNumero=parseInt(segundoNumero);
+
+
+	suma= primerNumero+segundoNumero;
+	alert(suma);
 
 }
 
