6dfbb2535d16d69e70dd217fff0bdf378f02e685
Jose Benavente <60740213+JoseFL1999@users.noreply.github.com>
2020-02-07 12:24:41 -0300

    Parcial 3 hecho

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,19 @@
 function mostrar()
 {
+	var precio;
+	var porcentaje;
+	var precioFinal;
+	var descuento;
 
+	precio = prompt("Coloque el precio");
+	console.log("el precio es " +precio);
+	porcentaje= prompt("Coloque el % de descuento");
+	console.log("El porcentaje de descuento es "+ porcentaje);
+
+	descuento= precio*porcentaje/100;
+
+	precioFinal= precio - descuento;
+	document.getElementById('elPrecioFinal').value=precioFinal;
+
+	console.log("el precio final es "+ precioFinal);
 }
