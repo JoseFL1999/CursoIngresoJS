57ffc101b55d1842353bf9f3071bfad65b88239f
alumno <benavente.21.05@gmail.com>
2020-02-13 11:26:02 -0300

    else

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,21 +1,17 @@
 function mostrar()
 {
 	var edad;
-
+	//tomo la edad
 	edad = document.getElementById('edad').value;
-	edad = parseInt(edad);
-	//tomo la edad 
+	edad = parseInt(edad);  
+   	if(edad > 17)
+   	{
+   		alert("Usted es mayor de edad");
+   	}
 
-	edad = edad > 17
-
-	if(edad)
-	{
-		alert("Usted es mayor de edad");
-	} 
-
-	else
-	{
-		alert("Usted es menor de edad");
-	}
+   	else
+   	{
+   		alert("Usted es menor de edad");
+   	}
 
 }//FIN DE LA FUNCIÓN
