415e85e86191174f487c41971c83bf5b795ce5fd
alumno <benavente.21.05@gmail.com>
2020-02-13 11:01:49 -0300

    Aprendiendo If y else

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,21 @@
 function mostrar()
 {
-//tomo la edad  
+	var edad;
 
+	edad = document.getElementById('edad').value;
+	edad = parseInt(edad);
+	//tomo la edad 
+
+	edad = edad > 17
+
+	if(edad)
+	{
+		alert("Usted es mayor de edad");
+	} 
+
+	else
+	{
+		alert("Usted es menor de edad");
+	}
 
 }//FIN DE LA FUNCIÓN
