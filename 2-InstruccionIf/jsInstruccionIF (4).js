16783a9ad3b0f07201770a1dba52866f17cd4884
alumno <benavente.21.05@gmail.com>
2020-02-13 11:06:27 -0300

    Ejercicio if else

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,21 @@
 function mostrar()
 {
+	var edad;
+
+	edad = document.getElementById('edad').value;
+	edad = parseInt(edad);
+
+	edad = edad > 12 && edad <18
+
 //tomo la edad  
-
-
+	
+	if(edad)
+	{
+		alert("Usted es adolescente");
+	}
+	else
+	{
+		alert("Usted no es adolescente");
+	}
 
 }//FIN DE LA FUNCIÓN
