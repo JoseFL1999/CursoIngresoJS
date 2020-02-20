ac53acd6031f53b68a6fa2702bb90b2b84bdae38
alumno <alumno>
2020-02-20 09:51:10 -0300

    S4

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,22 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño;
+	mesDelAño=document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
-	
-
-
-
+	switch(mesDelAño)
+	{
+		case "Febrero":
+			alert("Tiene 28 dias");
+			break;
+		case "Abril":
+		case "Junio":
+		case "Septiembre":
+		case "Noviembre":
+			alert("Tiene 30 dias");
+			break;
+		default:
+			alert("Tiene 31 dias");
+			break;
+	}
 }//FIN DE LA FUNCIÓN
