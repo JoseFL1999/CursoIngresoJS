86bdef9989db9b63b454fc24f77f4606257f76c4
alumno <alumno>
2020-02-27 11:30:40 -0300

    primo

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,7 +1,20 @@
 function mostrar()
-{
-
-
+{	var numeroIngresado;
+	var i;
+	var bandera;
+	bandera=0;
+	numeroIngresado=prompt("Ingrese un numero");
+	for(i=2;i<numeroIngresado;i++)
+	{
+		if(numeroIngresado%i==0)
+		{
+			bandera=1;
+		}
+	}
+	if(bandera!=1)
+	{
+		alert("Es primo el "+numeroIngresado);
+	}
 
 
 }//FIN DE LA FUNCIÓN
